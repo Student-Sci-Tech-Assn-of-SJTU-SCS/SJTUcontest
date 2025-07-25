c69fcfe07627077e3d4d4d6f5c645726987a2525
jiahang_wu <jiahang_wu@sjtu.edu.cn>
2025-07-25 20:45:03 +0800

    feat(frontend, matches): add circular progress when loading

--- frontend/src/pages/match/Matches.jsx
+++ frontend/src/pages/match/Matches.jsx
@@ -4,7 +4,7 @@ import {
   Grid,
   Divider,
   Pagination,
-  cardClasses,
+  CircularProgress
 } from "@mui/material";
 import MatchSearchBar from "../../components/MatchSearchBar";
 import MatchCard from "../../components/MatchCard";
@@ -17,112 +17,7 @@ import { createTheme } from "@mui/material/styles";
 
 import axios from "axios";
 import api from "../../utils/api";
-
-// const randomUUID = () =>
-//   "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
-//     const r = (Math.random() * 16) | 0;
-//     const v = c === "x" ? r : (r & 0x3) | 0x8;
-//     return v.toString(16);
-//   });
-
-// const getRandomLogo = (name) => {
-//   return `https://ui-avatars.com/api/?name=${encodeURIComponent(name)}&size=200&background=random&color=ffffff&bold=true`;
-// };
-
-// const getRandomTags = (arr, n = 1) => {
-//   const shuffled = arr.slice().sort(() => 0.5 - Math.random());
-//   return shuffled.slice(0, n).map(tag => tag.description);
-// };
-
-// const ALL_MATCHES = Array.from([
-//   {
-//     name: "2026年SJTU野外生存挑战赛",
-//     logo: getRandomLogo("Survival Challenge"),
-//     keywords: ["AI", "其他"],
-//   },
-//   {
-//     name: "2026年SJTU编程马拉松",
-//     logo: getRandomLogo("Programming Marathon"),
-//     keywords: ["CS", "math"],
-//   },
-//   {
-//     name: "2026年SJTU机器人竞赛",
-//     logo: getRandomLogo("Robotics Competition"),
-//     keywords: ["EE"],
-//   },
-//   {
-//     name: "2026年SJTU电子竞技大赛",
-//     logo: getRandomLogo("E-sports Competition"),
-//     keywords: ["其他"],
-//   },
-//   {
-//     name: "2026年SJTU人工智能挑战赛",
-//     logo: getRandomLogo("AI Challenge"),
-//     keywords: ["AI", "CS"],
-//   },
-//   {
-//     name: "2026年SJTU数学建模竞赛",
-//     logo: getRandomLogo("Math Modeling Competition"),
-//     keywords: ["math"],
-//   },
-//   {
-//     name: "2026年SJTU物理实验竞赛",
-//     logo: getRandomLogo("Physics Experiment Competition"),
-//     keywords: ["其他"],
-//   },
-//   {
-//     name: "2026年SJTU化学实验竞赛",
-//     logo: getRandomLogo("Chemistry Experiment Competition"),
-//     keywords: ["其他"],
-//   },
-//   {
-//     name: "2026年SJTU生物实验竞赛",
-//     logo: getRandomLogo("Biology Experiment Competition"),
-//     keywords: ["其他"],
-//   },
-//   {
-//     name: "第十八届全国大学生信息安全竞赛—作品赛",
-//     logo: getRandomLogo("18th National College InfoSec Competition - Project"),
-//     keywords: ["IS", "CS"],
-//   },
-//   {
-//     name: "第十八届全国大学生信息安全竞赛（创新实践能力赛）暨第二届“长城杯”铁人三项赛（防护赛）初赛",
-//     logo: getRandomLogo(
-//       "18th National College InfoSec Competition - Innovation & 2nd Great Wall Cup",
-//     ),
-//     keywords: ["IS", "CS"],
-//   },
-//   {
-//     name: "2026年SJTU操作系统原理知识竞赛",
-//     logo: getRandomLogo("OS Principles Competition"),
-//     keywords: ["CS"],
-//   },
-//   {
-//     name: "2026年SJTU计算机网络知识竞赛",
-//     logo: getRandomLogo("Computer Networks Competition"),
-//     keywords: ["CS"],
-//   },
-//   {
-//     name: "2026年SJTU数据库系统知识竞赛",
-//     logo: getRandomLogo("Database Systems Competition"),
-//     keywords: ["CS"],
-//   },
-//   {
-//     name: "2026年SJTU软件工程知识竞赛",
-//     logo: getRandomLogo("Software Engineering Competition"),
-//     keywords: ["CS"],
-//   },
-// ]).map((match) => {
-//   // 随机为每个比赛添加LEVEL/QUAL/YEAR标签
-//   const levelTags = getRandomTags(Object.values(categoryTags[categories.LEVEL]) || [], 1);
-//   const qualTags = getRandomTags(Object.values(categoryTags[categories.QUAL]) || [], 1);
-//   const yearTags = getRandomTags(Object.values(categoryTags[categories.YEAR]) || [], 1);
-//   return {
-//     ...match,
-//     uuid: randomUUID(),
-//     keywords: [...levelTags, ...qualTags, ...match.keywords, ...yearTags],
-//   };
-// });
+import { contestAPI } from "../../services/MatchServices";
 
 const Matches = () => {
   const [search, setSearch] = useState("");
@@ -144,7 +39,7 @@ const Matches = () => {
   const lg = useMediaQuery(theme.breakpoints.between("lg", "xl"));
   const xl = useMediaQuery(theme.breakpoints.up("xl"));
 
-  const pageSize = md ? 5 : 10;
+  const pageSize = sm ? 5 : 10;
   // const pageSize = sm ? 3 : md ? 6 : lg ? 9 : 12;
   // const pageSize = 1; // 调试用
 
@@ -158,33 +53,42 @@ const Matches = () => {
       setError("");
 
       try {
-        const res = await api.post(
-          "/matches/",
-          {
-            page_index: pageIndex,
-            page_size: pageSize,
-            options: {
-              query: search.trim(),
-              level: selectedTags[categories.LEVEL].map((tag) => tag.name),
-              quality: selectedTags[categories.QUAL].map((tag) => tag.name),
-              keywords: selectedTags[categories.KWORD].map((tag) => tag.name),
-              years: selectedTags[categories.YEAR].map((tag) => tag.name),
-              months: selectedTags[categories.MONTH].map((tag) => tag.name),
-            },
-          },
-          { signal: controller.signal },
-        );
+        // const res = await api.post(
+        //   "/matches/",
+        //   {
+        //     page_index: pageIndex,
+        //     page_size: pageSize,
+        //     options: {
+        //       query: search.trim(),
+        //       level: selectedTags[categories.LEVEL].map((tag) => tag.name),
+        //       quality: selectedTags[categories.QUAL].map((tag) => tag.name),
+        //       keywords: selectedTags[categories.KWORD].map((tag) => tag.name),
+        //       years: selectedTags[categories.YEAR].map((tag) => tag.name),
+        //       months: selectedTags[categories.MONTH].map((tag) => tag.name),
+        //     },
+        //   },
+        //   { signal: controller.signal },
+        // );
+
+        const res = await contestAPI.getContests(pageIndex, pageSize, {
+          query: search.trim(),
+          level: selectedTags[categories.LEVEL].map((tag) => tag.name),
+          quality: selectedTags[categories.QUAL].map((tag) => tag.name),
+          keywords: selectedTags[categories.KWORD].map((tag) => tag.name),
+          years: selectedTags[categories.YEAR].map((tag) => tag.name),
+          months: selectedTags[categories.MONTH].map((tag) => tag.name),
+        });
 
         if (res.success) {
           setMatches(res.data.matches || []);
           setPageCount(res.data.total_pages);
         } else {
-          setError(res.message || "未知错误");
+          setError(res.message || "Unknown error.");
           console.log(error);
         }
       } catch (err) {
         if (axios.isCancel(err)) return;
-        setError("网络错误");
+        setError("Network error.");
       } finally {
         setLoading(false);
       }
@@ -195,32 +99,12 @@ const Matches = () => {
     fetchMatches();
   }, [pageIndex, pageSize, search, selectedTags]);
 
-  // 模拟后端请求
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     let filtered = ALL_MATCHES.filter((match) => {
-  //       const nameMatch = match.name.includes(search);
-
-  //       const tagMatch = Reflect.ownKeys(selectedTags).every((category) => {
-  //         const selected = selectedTags[category];
-  //         if (selected.length === 0) return true;
-  //         return match.keywords.some((tag) => selected.includes(tag));
-  //       });
-
-  //       return nameMatch && tagMatch;
-  //     });
-  //     setMatches(filtered);
-  //   }, 200);
-  //   return () => clearTimeout(timer);
-  // }, [search, selectedTags]);
-
   const handleSearchChange = (newSearch) => {
     setSearch(newSearch);
     setPageIndex(1);
   };
 
   const handleTagClick = (tag) => {
-    // console.log(`Calling handleTagClick(${tag}) ...`);
     const category = tag.category;
     setSelectedTags((prev) => {
       const cur = prev[category];
@@ -232,7 +116,6 @@ const Matches = () => {
       );
       return { ...prev, [category]: upd };
     });
-    // console.log(`selectedTags=${selectedTags[category] && []}`);
     setPageIndex(1);
   };
 
@@ -265,43 +148,51 @@ const Matches = () => {
         onTagClick={handleTagClick}
       />
 
-      <Grid container spacing={1} justifyContent="center" alignItems="stretch">
-        {matches.length === 0 ? (
-          <Grid key={"no_match"} size={12}>
-            <Typography
-              key={"no_match"}
-              color="text.secondary"
-              align="center"
-              sx={{ mt: 8 }}
-            >
-              暂无符合条件的比赛
-            </Typography>
-          </Grid>
-        ) : (
-          matches.map((match) => (
-            <Grid
-              key={match.id}
-              // size={{ xs: 12, sm: 12, md: 6, lg: 4, xl: 3 }}
-              size={{ sm: 12, md: 6 }}
-              display="flex"
-              justifyContent="center"
-            >
-              <MatchCard match={match} />
-            </Grid>
-          ))
-        )}
-      </Grid>
-
-      {pageCount > 1 && (
-        <Box display="flex" justifyContent="center" mt={4}>
-          <Pagination
-            count={pageCount}
-            page={pageIndex}
-            onChange={(_, value) => setPageIndex(value)}
-            color="primary"
-            shape="rounded"
-          />
+      { loading ? (
+        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 8 }}>
+          <CircularProgress />
         </Box>
+      ) : (
+        <>
+          <Grid container spacing={1} justifyContent="center" alignItems="stretch">
+            {matches.length === 0 ? (
+              <Grid key={"no_match"} size={12}>
+                <Typography
+                  key={"no_match"}
+                  color="text.secondary"
+                  align="center"
+                  sx={{ mt: 8 }}
+                >
+                  暂无符合条件的比赛
+                </Typography>
+              </Grid>
+            ) : (
+              matches.map((match) => (
+                <Grid
+                  key={match.id}
+                  // size={{ xs: 12, sm: 12, md: 6, lg: 4, xl: 3 }}
+                  size={{ sm: 12, md: 6 }}
+                  display="flex"
+                  justifyContent="center"
+                >
+                  <MatchCard match={match} />
+                </Grid>
+              ))
+            )}
+          </Grid>
+
+          {pageCount > 1 && (
+            <Box display="flex" justifyContent="center" mt={4}>
+              <Pagination
+                count={pageCount}
+                page={pageIndex}
+                onChange={(_, value) => setPageIndex(value)}
+                color="primary"
+                shape="rounded"
+              />
+            </Box>
+          )}
+        </>
       )}
     </Box>
   );
