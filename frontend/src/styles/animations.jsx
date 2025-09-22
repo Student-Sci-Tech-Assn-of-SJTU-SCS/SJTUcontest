801cd3887ceb0159fac7531ed7a745d08722f5d9
jiahang_wu <jiahang_wu@sjtu.edu.cn>
2025-09-22 22:35:00 +0800

    feat(frontend): add animation createFadeOutAnim so as to configure fade-out animations

--- frontend/src/styles/animations.jsx
+++ frontend/src/styles/animations.jsx
@@ -35,3 +35,41 @@ export const createFadeInAnim = ({
     },
   };
 };
+
+export const createFadeOutAnim = ({
+  name = "fadeOut",
+  direction = "", // 可选：down, up, left, right，默认为空
+  distance = 20,
+  fromOpacity = 1,
+  toOpacity = 0,
+} = {}) => {
+  let transformFrom = "";
+  switch (direction) {
+    case "down":
+      transformFrom = `translateY(${distance}px)`;
+      break;
+    case "up":
+      transformFrom = `translateY(-${distance}px)`;
+      break;
+    case "left":
+      transformFrom = `translateX(${distance}px)`;
+      break;
+    case "right":
+      transformFrom = `translateX(-${distance}px)`;
+      break;
+    default:
+      transformFrom = "none";
+  }
+  return {
+    [`@keyframes ${name}`]: {
+      "0%": {
+        opacity: fromOpacity,
+        transform: "none",
+      },
+      "100%": {
+        opacity: toOpacity,
+        transform: transformFrom,
+      },
+    },
+  };
+};
