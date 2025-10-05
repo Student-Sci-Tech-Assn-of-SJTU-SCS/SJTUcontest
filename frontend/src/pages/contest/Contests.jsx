cef468f1944d61696a6f86363642dcaeff7cbd42
jiahang_wu <jiahang_wu@sjtu.edu.cn>
2025-10-05 15:36:22 +0800

    feat(frontend): add react-hotkeys-hook for keyboard shortcuts in ContestSearchBar

--- frontend/src/pages/contest/Contests.jsx
+++ frontend/src/pages/contest/Contests.jsx
@@ -32,7 +32,6 @@ const Contests = () => {
   const [pageIndex, setPageIndex] = useState(1);
   const [pageCount, setPageCount] = useState(0);
   const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState("");
 
   const sm = useMediaQuery(theme.breakpoints.down("md"));
   const md = useMediaQuery(theme.breakpoints.between("md", "lg"));
@@ -60,20 +59,20 @@ const Contests = () => {
           quality: selectedTags[categories.QUAL].map((tag) => tag.name),
           keywords: selectedTags[categories.KWORD].map((tag) => tag.name),
           years: selectedTags[categories.YEAR].map((tag) => tag.name),
+
           months: selectedTags[categories.MONTH].map((tag) => tag.name),
         });
 
-        if (res.success) {
+        if (res.data.success) {
           setContests(res.data.matches || []);
           setPageCount(res.data.total_pages);
         } else {
           showMessage(`获取比赛失败：${res.message || "未知错误。"}`, "error");
-          // setError(res.message || "Unknown error.");
         }
       } catch (err) {
-        if (axios.isCancel(err)) return;
+        console.error(err);
+        // if (axios.isCancel(err)) return;
         showMessage("获取比赛失败：网络错误。", "error");
-        // setError("Network error.");
       } finally {
         setLoading(false);
       }
@@ -114,7 +113,7 @@ const Contests = () => {
       [categories.MONTH]: [],
     });
     setPageIndex(1);
-  }
+  };
 
   useEffect(() => {
     setPageIndex(1);
@@ -191,6 +190,9 @@ const Contests = () => {
             spacing={3}
             justifyContent="center"
             alignItems="stretch"
+            sx={{
+              transition: "all ease-in-out 0.5s",
+            }}
           >
             {contests.length === 0 ? (
               <Grid size={{ xs: 12 }}>
